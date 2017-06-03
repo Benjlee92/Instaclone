67fb56751e6bee1bec0b32832521aa07b19c5fc6
Benjamin Lee <benjlee92@gmail.com>
2017-06-02 22:03:04 -0700

    finished the first login screen UI

---
+++ Instaclone/BlurredView.swift
@@ -0,0 +1,21 @@
+//
+//  BlurredView.swift
+//  Instaclone
+//
+//  Created by Ben on 6/2/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class BlurredView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Instaclone/MovingBackground.swift
@@ -0,0 +1,21 @@
+//
+//  MovingBackground.swift
+//  Instaclone
+//
+//  Created by Ben on 6/2/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class MovingBackground: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
